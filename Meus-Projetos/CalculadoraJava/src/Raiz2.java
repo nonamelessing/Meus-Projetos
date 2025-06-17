64fa609e4877afc0462918e2462254bf249e185f
nonamelessing <vn2616003@gmail.com>
2025-06-17 14:57:33 -0300

    Delete Meus-Projetos directory

--- Meus-Projetos/CalculadoraJava/src/Raiz2.java
+++
@@ -1,9 +0,0 @@
-import java.math.*;
-public class Raiz2 implements Conta {
-	
-	@Override
-	public double Conta(double a, double b) {
-		return Math.sqrt(b);
-	}
-
-}
--- Meus-Projetos/SpringBoot/ProjetoFinal/src/main/java/br/com/projeto/ProjetoFinal/repository/IUsuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.projeto.ProjetoFinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.projeto.ProjetoFinal.model.UsuarioModel;
-
-public interface IUsuario extends JpaRepository<UsuarioModel, Integer> {
-    UsuarioModel findByEmail(String email);
-}
